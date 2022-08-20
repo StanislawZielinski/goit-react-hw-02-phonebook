6a3d5a880c2d0879bcdccf3ec4fe31223607e792
StanislawZielinski <stanislaw.zielinski.poczta@gmail.com>
2022-08-20 15:33:00 +0200

    test

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-class Contacts extends Component {
+class Filter extends Component {
 
     render() {
         return (
@@ -8,9 +8,8 @@ class Contacts extends Component {
                 <p className="contacts-title">Contacts</p>
                 <p className="filter-title">filter contacts by name</p>
                 <input className="input" onChange={this.props.onChange} />
-                <ul>{this.props.renderContacts(this.props.filter, this.props.contacts)}</ul>
-                </div>
+            </div>
     )}
 }
 
-export default Contacts
+export default Filter
--- src/components/phonebook/Phonebook.jsx
+++ src/components/phonebook/Phonebook.jsx
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 import { nanoid } from 'nanoid';
 import styles from "components/phonebook/Phonebook.css"
 import Form from "components/Form/Form";
-import Contacts from "components/Contacts/Contacts"
+// import Contacts from "components/Contacts/Contacts"
+import Filter from "components/Filter/Filter"
 
 class Phonebook extends Component {
     state = {
         contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-  ],
+            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+        ],
         filter: '',
     }
     handleSubmit = (evt) => {
@@ -34,17 +35,15 @@ class Phonebook extends Component {
             filterFunction.map(contact =>
             { return <li className="contacts" key={contact.id}>{contact.name}: {contact.number} </li> })
         )
-        
     }
+    onChange = (evt) => this.setState({ ...this.setState, filter: evt.target.value });
+
     render() {
         return (
             <div>
                 <Form handleSubmit={this.handleSubmit} />
-                {/* <Contacts /> */}
                 <div>
-                    <p className="contacts-title">Contacts</p>
-                    <p className="filter-title">filter contacts by name</p>
-                    <input className="input" onChange={(evt)=>this.setState({ ...this.setState, filter: evt.target.value })} />
+                <Filter onChange={this.onChange} />
                     <ul>{this.renderContacts(this.state.filter, this.state.contacts)}</ul>
                  </div>
             </div>
